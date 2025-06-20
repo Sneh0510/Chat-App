63ec08852804d7c3d04a879c98044d5dc62ec545
Sneh Yadav <snehyadav091@gmail.com>
2025-06-20 18:47:35 +0530

    Refactor background image path and replace SVG asset

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,7 +9,7 @@ import { Authcontext } from '../context/AuthContext'
 const App = () => {
   const {authUser} = useContext(Authcontext)
   return (
-    <div className="bg-[url('./src/assets/bgImage.svg')] bg-contain">
+    <div className="bg-[url('/bgImage.svg')] bg-contain">
       <Toaster />
       <Routes>
         <Route path='/' element={authUser ? <HomePage />:<Navigate to="/login"/>}/>
